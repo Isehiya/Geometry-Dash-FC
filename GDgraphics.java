ffba86aea8c36273a09e665c12fb4b8de647d09c
Isehiya <jacobjunxilin@gmail.com>
2025-05-28 16:11:43 -0400

    added moving background and GDblock image

--- GDgraphics.java
+++ GDgraphics.java
@@ -1,45 +1,61 @@
 import java.awt.*;
-import java.awt.geom.*;
 import javax.swing.*;
 
 public class GDgraphics extends JComponent {
     private int frameWidth;
     private int frameHeight;
+
     private Image playerImg;
     private Image spikeImg;
+    private Image bgImg;
+
+    private int bgX = 0;
+    private Timer timer;
+    private int scrollSpeed = 2;
 
     public GDgraphics(int w, int h) {
         frameWidth = w;
         frameHeight = h;
 
-        // Load images from project folder
+        // Load images
         playerImg = Toolkit.getDefaultToolkit().getImage("GDdefaulticon.png");
         spikeImg = Toolkit.getDefaultToolkit().getImage("GDspike.png");
+        bgImg = Toolkit.getDefaultToolkit().getImage("GDbackground.png");
+
+        // Start scrolling background
+        timer = new Timer(16, e -> {
+            bgX -= scrollSpeed;
+            if (bgX <= -frameWidth) {
+                bgX = 0;
+            }
+            repaint();
+        });
+        timer.start();
     }
 
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         Graphics2D g2d = (Graphics2D) g;
 
-        // Anti-aliasing
+        // Smooth drawing
         g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
-        // Background
-        g2d.setColor(new Color(30, 30, 30));
-        g2d.fillRect(0, 0, frameWidth, frameHeight);
+        // Scrolling background
+        g2d.drawImage(bgImg, bgX, 0, frameWidth, frameHeight, this);
+        g2d.drawImage(bgImg, bgX + frameWidth, 0, frameWidth, frameHeight, this);
 
         // Ground
-        g2d.setColor(new Color(60, 180, 75));
         int groundHeight = 100;
+        g2d.setColor(new Color(60, 180, 75));
         g2d.fillRect(0, frameHeight - groundHeight, frameWidth, groundHeight);
 
-        // Draw player image
+        // Player cube
         int cubeSize = 40;
         int cubeX = 100;
         int cubeY = frameHeight - groundHeight - cubeSize;
         g2d.drawImage(playerImg, cubeX, cubeY, cubeSize, cubeSize, this);
 
-        // Draw spike image
+        // Spike
         int spikeSize = 40;
         int spikeX = 300;
         int spikeY = frameHeight - groundHeight - spikeSize;
--- GDmain.java
+++ GDmain.java
@@ -7,9 +7,9 @@ public class GDmain {
 
         JFrame f = new JFrame();
         GDgraphics canvas = new GDgraphics(w, h);
+
         f.setSize(w, h);
         f.setTitle("Geometry Dash");
-
         f.add(canvas);
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         f.setVisible(true);
