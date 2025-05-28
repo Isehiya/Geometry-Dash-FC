3e63dbde770ae5fa7cd30de31f54dbf49c5feb42
Isehiya <jacobjunxilin@gmail.com>
2025-05-28 11:32:41 -0400

    gay commit

---
+++ GeometryDashMain.java
@@ -0,0 +1,17 @@
+import java.awt.*;
+import java.awt.geom.*;
+import javax.swing.*;
+
+public class GeometryDashMain extends JComponent{
+        private int frameWidth;
+        private int frameHeight;
+
+        public DrawingCanvas(int w, int h){
+            frameWidth = w;
+            frameHeight = h;
+        }
+        protected void paintComponent(Graphics g){
+
+        }
+
+}
