d33ca1503fe4e874ec4d3bf2ed714623f6012019
Isehiya <jacobjunxilin@gmail.com>
2025-06-04 11:16:54 -0400

    Isehiya - Editted

--- GDgraphics.java
+++ GDgraphics.java
@@ -1,22 +1,19 @@
 import javax.swing.*;
 import java.awt.*;
-import java.io.*;
 import java.awt.event.*;
-import java.awt.geom.AffineTransform;
-import java.awt.geom.Rectangle2D;
 import javax.sound.sampled.*;
-import java.util.*;
+import java.io.File;
+import java.io.IOException;
 
 public class GDgraphics extends JPanel implements KeyListener {
 
-    // --- Constants
-    public static final int WIDTH           = 800;
-    public static final int HEIGHT          = 600;
-    public static final int FPS_DELAY       = 16;      // ~60 FPS
-    public static final int GRAVITY         = 1;
-    public static final int JUMP_VELOCITY   = -15;
-    public static final int GROUND_Y        = 500;     // where player lands
-    private static final double ROT_SPEED    = 10.0;     // deg/frame
+    public static final int WIDTH = 800;
+    public static final int HEIGHT = 600;
+    public static final int FPS_DELAY = 16;
+    public static final int GRAVITY = 1;
+    public static final int JUMP_VELOCITY = -15;
+    public static final int GROUND_Y = 500;
+    private static final double ROT_SPEED = 10.0;
 
     public static final int SPIKE_HITBOX_WIDTH = 3;
     public static final int SPIKE_HITBOX_HEIGHT = 5;
@@ -24,18 +21,16 @@ public class GDgraphics extends JPanel implements KeyListener {
     public static int playerSize = 40;
     public static int scrollSpeed = 5;
 
-    // --- State
-    public static Rectangle2D.Double player;
-    public Rectangle2D.Double[] blocks = new Rectangle2D.Double[10000];
-    public Rectangle2D.Double[] spikes = new Rectangle2D.Double[10000];
+    public static Rectangle player;
+    public Rectangle[] blocks = new Rectangle[10000];
+    public Rectangle[] spikes = new Rectangle[10000];
 
-    private int velocityY    = 0;
-    private boolean isJump   = false;
-    private double rotation  = 0;
+    private int velocityY = 0;
+    private boolean isJump = false;
+    private double rotation = 0;
 
-    Clip backgroundMusic1, backgroundMusic2;
+    Clip backgroundMusic1;
 
-    // --- Images
     private Image playerImg;
     private Image blockImg;
     private Image bgImg;
@@ -43,7 +38,6 @@ public class GDgraphics extends JPanel implements KeyListener {
 
     private Image halfSpeedPortal, speedPortal1, speedPortal2, speedPortal3, speedPortal4;
 
-    // --- Scroll
     private int bgOffsetX = 0;
     private boolean running = false;
 
@@ -66,39 +60,31 @@ public class GDgraphics extends JPanel implements KeyListener {
         tracker.addImage(speedPortal4, 4);
         playerImg = Toolkit.getDefaultToolkit().getImage("GDdefaulticon.png");
         tracker.addImage(playerImg, 5);
-        blockImg  = Toolkit.getDefaultToolkit().getImage("GDblock.png");
+        blockImg = Toolkit.getDefaultToolkit().getImage("GDblock.png");
         tracker.addImage(blockImg, 6);
-        bgImg     = Toolkit.getDefaultToolkit().getImage("GDbackground.png");
+        bgImg = Toolkit.getDefaultToolkit().getImage("GDbackground.png");
         tracker.addImage(bgImg, 7);
         spike = Toolkit.getDefaultToolkit().getImage("GDspike.png");
         tracker.addImage(spike, 8);
-        try{
+        try {
             tracker.waitForAll();
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-        player = new Rectangle2D.Double(50, GROUND_Y, playerSize, playerSize);
 
-        try{
+        player = new Rectangle(50, GROUND_Y, playerSize, playerSize);
+
+        try {
             AudioInputStream sound = AudioSystem.getAudioInputStream(new File("StereoMadness.wav"));
             backgroundMusic1 = AudioSystem.getClip();
             backgroundMusic1.open(sound);
+        } catch (Exception e) {}
 
-        } catch (Exception e) {
-
-        }
         backgroundMusic1.setFramePosition(0);
         backgroundMusic1.loop(Clip.LOOP_CONTINUOUSLY);
 
-        spikes[3] = new Rectangle2D.Double(1000, 500, 10, 20);
-
-        for (int i = 0; i < spikes.length; i++) {
-
-        }
-
-    } // Constructor
-
-
+        spikes[3] = new Rectangle(1000, 500, 10, 20);
+    }
 
     public void startGameLoop() {
         running = true;
@@ -117,29 +103,29 @@ public class GDgraphics extends JPanel implements KeyListener {
     }
 
     private void updateGame() {
-        // Scroll background
         if (bgImg != null) {
             bgOffsetX = (bgOffsetX - scrollSpeed) % bgImg.getWidth(this);
         }
-        if (player.intersects(spikes[3])){
+
+        if (spikes[3] != null && player.intersects(spikes[3])) {
             System.exit(0);
         }
-        spikes[3].x -= scrollSpeed;
 
-        // Apply gravity
+        if (spikes[3] != null) {
+            spikes[3].x -= scrollSpeed;
+        }
+
         velocityY += GRAVITY;
-        player.y   += velocityY;
+        player.y += velocityY;
 
-        // Rotate mid-air
         if (isJump) {
             rotation = (rotation + ROT_SPEED) % 360;
         }
 
-        // Land on ground
         if (player.y > GROUND_Y) {
-            player.y   = GROUND_Y;
-            velocityY  = 0;
-            isJump     = false;
+            player.y = GROUND_Y;
+            velocityY = 0;
+            isJump = false;
             if (rotation <= 45)
                 rotation = 0;
             else if (rotation > 45 && rotation <= 135)
@@ -158,7 +144,7 @@ public class GDgraphics extends JPanel implements KeyListener {
         super.paintComponent(g);
         Graphics2D g2 = (Graphics2D) g;
 
-        // 1) Draw scrolling background
+        // Draw scrolling background
         if (bgImg != null) {
             int bwBg = bgImg.getWidth(this);
             for (int i = -1; i <= getWidth() / bwBg + 1; i++) {
@@ -166,26 +152,25 @@ public class GDgraphics extends JPanel implements KeyListener {
             }
         }
 
-        for (int i = 0; i < spikes.length; i++){
-            int spikeHeight = spike.getHeight(this);
-            int spikeWidth = spike.getWidth(this);
-            spikeWidth /= 3;
-            spikeHeight /= 3;
-            if (spikes[i] != null){
-                g2.drawImage(spike, (int) spikes[3].x, (int) spikes[3].y, spikeWidth, spikeHeight, this);
+        // Draw spikes
+        for (int i = 0; i < spikes.length; i++) {
+            if (spikes[i] != null) {
+                int spikeHeight = spike.getHeight(this) / 3;
+                int spikeWidth = spike.getWidth(this) / 3;
+                g2.drawImage(spike, spikes[i].x, spikes[i].y, spikeWidth, spikeHeight, this);
                 g2.setColor(Color.RED);
-                g2.drawRect((int) spikes[3].x, (int) spikes[3].y, 10, 20);
+                g2.drawRect(spikes[i].x, spikes[i].y, spikes[i].width, spikes[i].height);
             }
         }
 
-        // 2) Draw three stacked floor tiles exactly filling gap under player
+        // Draw floor tiles
         if (blockImg != null) {
             int nativeBw = blockImg.getWidth(this);
             int nativeBh = blockImg.getHeight(this);
             int gapHeight = getHeight() - (GROUND_Y + playerSize);
             int scaledBh = gapHeight / 3;
             int scaledBw = nativeBw * scaledBh / nativeBh;
-            int startY = GROUND_Y + playerSize;  // top of top block
+            int startY = GROUND_Y + playerSize;
             int offsetX = bgOffsetX % scaledBw;
             for (int row = 0; row < 3; row++) {
                 int y = startY + row * scaledBh;
@@ -195,41 +180,40 @@ public class GDgraphics extends JPanel implements KeyListener {
             }
         }
 
-        // 3) Draw and rotate player icon
+        // Draw and rotate player icon
         if (playerImg != null) {
-            AffineTransform old = g2.getTransform();
-            double cx = player.x + playerSize / 2.0;
-            double cy = player.y + playerSize / 2.0;
+            double cx = player.x + player.width / 2.0;
+            double cy = player.y + player.height / 2.0;
             g2.rotate(Math.toRadians(rotation), cx, cy);
-            g2.drawImage(playerImg, (int) player.x, (int) player.y, playerSize, playerSize, null);
-            g2.setTransform(old);
+            g2.drawImage(playerImg, player.x, player.y, player.width, player.height, null);
+            g2.rotate(-Math.toRadians(rotation), cx, cy); // reset rotation
         } else {
             g2.setColor(Color.BLUE);
-            g2.fill(player);
+            g2.fillRect(player.x, player.y, player.width, player.height);
         }
 
-        // 4) Debug overlay
+        // Debug info
         g2.setColor(new Color(0, 0, 0, 200));
         g2.fillRect(10, 10, 200, 125);
         g2.setColor(Color.WHITE);
         g2.setFont(new Font("Monospaced", Font.PLAIN, 12));
         int line = 0;
-        g2.drawString(String.format("player.x   = %.1f", player.x), 15, 30 + line * 15); line++;
-        g2.drawString(String.format("player.y   = %.1f", player.y), 15, 30 + line * 15); line++;
+        g2.drawString(String.format("player.x   = %d", player.x), 15, 30 + line * 15); line++;
+        g2.drawString(String.format("player.y   = %d", player.y), 15, 30 + line * 15); line++;
         g2.drawString("velocityY  = " + velocityY, 15, 30 + line * 15); line++;
         g2.drawString("isJump     = " + isJump, 15, 30 + line * 15); line++;
         g2.drawString(String.format("rotation   = %.1f", rotation), 15, 30 + line * 15); line++;
         g2.drawString("bgOffsetX  = " + bgOffsetX, 15, 30 + line * 15);
     }
 
-    // --- KeyListener methods
     public void keyPressed(KeyEvent e) {
         int code = e.getKeyCode();
         if ((code == KeyEvent.VK_SPACE || code == KeyEvent.VK_UP) && !isJump) {
             velocityY = JUMP_VELOCITY;
-            isJump   = true;
+            isJump = true;
         }
     }
+
     public void keyReleased(KeyEvent e) {}
     public void keyTyped(KeyEvent e) {}
 }
--- GDmain.java
+++ GDmain.java
@@ -15,4 +15,6 @@ public class GDmain {
 
         panel.startGameLoop();
     }
+
+
 }
